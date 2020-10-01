4bae3ae70630a6cd41f5ca42e1df1b62ca7ced0a
Elzara20 <71425807+Elzara20@users.noreply.github.com>
2020-10-01 11:54:14 +0300

    Update kwNN.R

--- kwNN.R
+++ kwNN.R
@@ -1,44 +1,71 @@
 Euclid <- function(u, v){
     return(sqrt(sum((u - v)^2)))
 }
-kwNN <- function(xl, z, k){
+kwNN <- function(xl,z,k,q){
     l <- dim(xl)[1] # строки
-    n <- dim(xl)[2] - 1 # столбцы 
+    n <- dim(xl)[2]-1 # столбцы 
     dist <-c(0)
-    weight1_setosa <- 0
-    weight2_versicolor <- 0
-    weight3_virginica <- 0
-
+    colors <- c("setosa" = 0, "versicolor" = 0, "virginica" = 0)
+   
     for (i in 1:l){
-        dist[i] <- c(Euclid(xl[i, 1:n], z))
+        dist[i] <- c(Euclid(xl[i, 1:n], z[,1:n]))
     }
     #сортировка выборки по расстоянию
     orderedXl <- xl[order(dist), ]
     
-    for (i in 1:k){
-       
-        if(orderedXl[i,3]=='setosa'){
-            weight1_setosa=weight1_setosa+(1/dist[i])^2 # взвешенная сумма для класса 'setosa'
-        }else if (orderedXl[i,3]=='versicolor'){
-            weight2_versicolor=weight2_versicolor+(1/dist[i])^2 # взвешенная сумма для класса 'versicolor'
-        }else if (orderedXl[i,3]=='virginica'){
-            weight3_virginica=weight3_virginica+(1/dist[i])^2  # взвешенная сумма для класса 'virginica'
+    for (c in names(colors)){ 
+        for (i in 1:k){            
+            if(orderedXl[i,3]==c){
+                colors[c] <- colors[c]+q^i # взвешенная сумма для k ближайших соседей
+            }
+            
         }
+        
+        
     }
-    if (max(weight1_setosa,weight2_versicolor,weight3_virginica)==weight1_setosa){
-        return("setosa")
-    }else if (max(weight1_setosa,weight2_versicolor,weight3_virginica)==weight2_versicolor){
-        return("versicolor")
-    }else{
-        return("virginica")
-    }
+    #print(colors)
+    return(names(which.max(colors)))
    
 }
+LOO <- function(a){
+    len <- dim(a)[1]
+    ans <- c()
+    sum <- 0
+     for (j in 1:len){ # paccматриваем всех соседей
+        sum <- 0
+        for (i in 1:len){  # рассматриваем  случайные данные из iris 
+            z <- a[i, 1:2] # координаты
+            cl <- kwNN(a[-i,], z,6, (j/10)) # применяем метод ближайших соседей
+            # если классы не совпадают, то прибавляем  1      
+            if (a[i, 3] != cl){
+                
+                sum <- sum + 1
+            }   
+              
+    
+        }       
+        ans <- c(ans, sum/len)  #записываем вероятность появления ошибки при каждом k (число соседей)    
+        #jk <- j
+    }
+    #print(min(ans))
+    for (j in 1:10){
+        if (ans[j]==min(ans)){
+            kj <- j
+        }
+    }
+    print(kj)
+    print(ans)
+    plot(1:len,ans, type="l")
+    
+}
 colors <- c("setosa" = "red", "versicolor" = "green", "virginica" = "blue")
-plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species], asp = 1)
-z <- c(runif(1, 0.9, 7), runif(1, 0, 2.5))
-xl <- iris[, 3:5]
 
-k <- readline(prompt = "k = ")
-class1 <- kwNN(xl, z, k)
-points(z[1], z[2], pch = 24, bg = colors[class1])
+plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species], asp = 1)
+z <- iris[67,3:5]
+xl <- iris[-67, 3:5]
+a <- iris[,3:5]
+#k <- readline(prompt = "k = ")
+#kwNN(xl,z,6,0.1)
+LOO(a)
+#class1 <- kwNN(xl,k)
+#points(z[1], z[2], pch = 24, bg = colors[class1])
