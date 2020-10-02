b86125f43bce2acc1d58a43bacb59f535db62d9b
Elzara20 <71425807+Elzara20@users.noreply.github.com>
2020-10-02 08:29:01 +0300

    Update kwNN.R

--- kwNN.R
+++ kwNN.R
@@ -1,7 +1,7 @@
 Euclid <- function(u, v){
     return(sqrt(sum((u - v)^2)))
 }
-kwNN <- function(xl,z,k,q){
+KwNN <- function(xl,z,k,q){
     l <- dim(xl)[1] # строки
     n <- dim(xl)[2]-1 # столбцы 
     dist <-c(0)
@@ -13,17 +13,14 @@ kwNN <- function(xl,z,k,q){
     #сортировка выборки по расстоянию
     orderedXl <- xl[order(dist), ]
     
-    for (c in names(colors)){ 
-        for (i in 1:k){            
+    for (c in names(colors)){ # имена классов
+        for (i in 1:k){     # соседи       
             if(orderedXl[i,3]==c){
                 colors[c] <- colors[c]+q^i # взвешенная сумма для k ближайших соседей
-            }
-            
-        }
-        
-        
+            }           
+        }     
     }
-    #print(colors)
+    
     return(names(which.max(colors)))
    
 }
@@ -31,11 +28,11 @@ LOO <- function(a){
     len <- dim(a)[1]
     ans <- c()
     sum <- 0
-     for (j in 1:len){ # paccматриваем всех соседей
+    for (j in 1:len){ # paccматриваем всех соседей
         sum <- 0
         for (i in 1:len){  # рассматриваем  случайные данные из iris 
             z <- a[i, 1:2] # координаты
-            cl <- kwNN(a[-i,], z,6, (j/10)) # применяем метод ближайших соседей
+            cl <- KwNN(a[-i,], z,6, (j/len)) # применяем метод ближайших соседей
             # если классы не совпадают, то прибавляем  1      
             if (a[i, 3] != cl){
                 
@@ -54,7 +51,7 @@ LOO <- function(a){
         }
     }
     print(kj)
-    print(ans)
+    #print(ans)
     plot(1:len,ans, type="l")
     
 }
@@ -64,8 +61,5 @@ plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species
 z <- iris[67,3:5]
 xl <- iris[-67, 3:5]
 a <- iris[,3:5]
-#k <- readline(prompt = "k = ")
-#kwNN(xl,z,6,0.1)
 LOO(a)
-#class1 <- kwNN(xl,k)
-#points(z[1], z[2], pch = 24, bg = colors[class1])
+
