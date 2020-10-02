f6930ad39033851fe6375564d84f72eb3f3ac91f
Elzara20 <71425807+Elzara20@users.noreply.github.com>
2020-10-02 08:26:15 +0300

    Update LOO.R

--- LOO.R
+++ LOO.R
@@ -1,7 +1,7 @@
 Euclid <- function(u, v){
     return(sqrt(sum((u - v)^2)))
 }
-kNN <- function(xl, z, k){
+KNN <- function(xl, z, k){
     l <- dim(xl)[1] # строки
     n <- dim(xl)[2] - 1 #  столбцы
     dist <-c(0)
@@ -10,13 +10,11 @@ kNN <- function(xl, z, k){
         dist[i] <- c(Euclid(xl[i, 1:n], z))
     }
     #сортировка выборки по расстоянию
-    orderedXl <- xl[order(dist), ]
-    #частота появления определенного цвета
-    counts <- table(orderedXl[1:k, n +1])
+    OrderedXl <- xl[order(dist), ]
+    #создаем таблицу
+    counts <- table(OrderedXl[1:k, n +1])
     # определение наиболее часто появляющегося класса
     class <- which.max(counts)
-    # cat("CLASS=", class)
-    # cat("Name= ", names(class))
     # возвращаем цвет наиболее близкого класса
     return (names(class))
 }
@@ -24,23 +22,28 @@ LOO <- function(a){
     len <- dim(a)[1]
     ans <- c()
     sum <- 0
-     for (j in 1:len-1){ # paccматриваем всех соседей
+     for (j in 1:len){ # paccматриваем всех соседей
         sum <- 0
         for (i in 1:len){  # рассматриваем  случайные данные из iris 
             z <- a[i, 1:2] # координаты
-            cl <- kNN(a[-i,], z, j) # применяем метод ближайших соседей
+            cl <- KNN(a[-i,], z, j) # применяем метод ближайших соседей
             # если классы не совпадают, то прибавляем  1      
-            if (a[i, 3] != cl){
-                
+            if (a[i, 3] != cl){                
                 sum <- sum + 1
-            }   
-              
+            }              
     
         }       
-        ans <- c(ans, sum/10)  #записываем вероятность появления ошибки при каждом k (число соседей)    
+        ans <- c(ans, sum/len)  #записываем вероятность появления ошибки при каждом k (число соседей)    
     }
-    plot(1:10,ans, type="l")
+    plot(1:len,ans, type="l")
+    kj <- 0
+     for (j in 1:10){
+        if (ans[j]==min(ans)){
+            kj <- j
+        }
+    }
+    print(kj)
     
 }
-a <- iris[sample(1:150,10),3:5] 
+a <- iris[, 3:5]
 LOO(a)
