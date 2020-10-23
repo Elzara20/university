e344a43dd1de79275cf5e84c4d60cdaffd414cc6
Elzara20 <71425807+Elzara20@users.noreply.github.com>
2020-10-23 08:23:42 +0300

    Changes in Parzen

--- Potential.R
+++ Potential.R
@@ -1,17 +1,15 @@
 Euclid <- function(u, v){
     return(sqrt(sum((u - v)^2)))
 }
-Sorted <- function(xl, z){
+Sorted <- function(xl){
     l <- dim(xl)[1] # строки
     n <- dim(xl)[2]-1 #  столбцы
-    dist <-matrix(0,l,2)
+    dist <-matrix(0,l,l)
 
-    # for (i in 1:l){
-    #     dist[i, ] <- c(Euclid(xl[i, 1:n], z[1:n]), xl[i,n+1])
-    # }
     for (i in 1:l){
-        for (j in 1:(len))
-        dist[j, i] <- c(Euclid(xl[i, 1:n], z[1:n]), xl[i,n+1])
+        for (j in 1:l){
+            dist[i,j] <- Euclid(xl[j, 1:n], xl[i,1:n])
+        }
     }
 
     #  OrderedXl <- dist[order(dist[, 1]), ]
@@ -20,7 +18,7 @@ Sorted <- function(xl, z){
     return(dist)
 }
 RectK <- function(dist) {
-  if (dist <= 1) {
+  if (abs(dist) <= 1) {
     return (0.5)
   } else {
     return (0.0)
@@ -32,28 +30,23 @@ Potetial <- function(xl, z, h,p){
     w <- c()
     colors <- c("setosa" = 0, "versicolor" = 0, "virginica" = 0)
     qw <- Sorted(xl[,1:3], z)
-    # print("qw")
-    # print(qw)
-   
-    #for (c in names(colors)){
-        for (i in 1:len){ 
-            r<-qw[i,1]/h
-            colors[qw[i,2]] <- colors[qw[i,2]]+p[i]*RectK(r) # взвешенная сумма для k ближайших соседей
+    for (i in 1:len){ 
+        r<-qw[i,1]/h
+        colors[qw[i,2]] <- colors[qw[i,2]]+p[i]*RectK(r) # взвешенная сумма для k ближайших соседей
                       
-        }     
-   # }
+    }     
     
-    return(names(which.max(colors)))
-   
+    return(names(which.max(colors)))  
 
 }
 LOO <- function(a){
+    colo <- c("setosa" = "coral1", "versicolor" = "chartreuse", "virginica" = "deepskyblue")
     len <- dim(a)[1]
     n <- dim(a)[2]
     ANS <- c()
     K<-0
     eps<-10
-    err<-eps+1
+    err<-eps+0.1
     h<-0.4 #оптимальное значение
     
     h_seq <- seq(0.1, 2, 0.1)
@@ -64,39 +57,44 @@ LOO <- function(a){
     for (i in 1:len){
         p[i]<-0
     }
-    while (err>eps){
+    qw<- matrix(0, len, len)
+    qw <- Sorted(a)
+    #print(qw)
+    while (err>eps && err<11){ #!! нужно ограничение для err
+        
         for (j in 1:len){ # paccматриваем случайные данные из iris
-            z <- a[j,1:3] # координаты
-            qw <- Sorted(a[-j,1:3], z)
+            # z <- a[j,1:3] # координаты
+            # qw <- Sorted(a[-j,1:3], z)
             colors <- c("setosa" = 0, "versicolor" = 0, "virginica" = 0)
-            for (i in 1:(len-1)){
-                r<-qw[i,1]/h
-                colors[qw[i,2]] <- colors[qw[i,2]]+p[i]*RectK(r) # взвешенная сумма   
+            for (i in 1:len){
+                r<-qw[j,i]/h
+                colors[a[i,3]] <- colors[a[i,3]]+p[i]*RectK(r) # взвешенная сумма   
             }
-                
-            if (a[j, 3] != names(which.max(colors)) && p[j]<6){  
-                p[i] <- p[i]+1 
-                err<-err+1
+                if (a[j, 3] != names(which.max(colors)) ){  
+                    p[j] <- p[j]+1          
                                             
-            }
+                }
+            # }
+        }
+                
             
                     
-        }
-        # for (j in 1:len){ # paccматриваем случайные данные из iris 
-        #     z <- a[j,1:3] # координаты
-        #     qw <- Sorted(a[-j,1:3], z)       
-        #     colors <- c("setosa" = 0, "versicolor" = 0, "virginica" = 0)
-        #     for (i in 1:(len-1)){                
-        #         r<-qw[i,1]/h
-        #         colors[qw[i,2]] <- colors[qw[i,2]]+p[i]*RectK(r) # взвешенная сумма для k ближайших соседей   
-        #     }
+        
+        for (j in 1:len){ # paccматриваем случайные данные из iris       
+            colors <- c("setosa" = 0, "versicolor" = 0, "virginica" = 0)
+            for (i in 1:(len-1)){                
+                r<-qw[j,i]/h
+                colors[a[i,3]] <- colors[a[i,3]]+p[i]*RectK(r) # взвешенная сумма   
+            }
                 
-        #     if (a[j, 3] != names(which.max(colors)) && p[j]<6){   
-        #         err<-err+1                                
-        #     }
+            if (a[j, 3] != names(which.max(colors))){   
+                err<-err+1                                
+            }
+        }
+        
             
                   
-    }
+    
     # err<-0
     # for (i in 1:len){
     #     if (a[i, n] != Potetial(a[-i,], a[i,], 0.4, p)){
@@ -105,13 +103,12 @@ LOO <- function(a){
     # }
     }
     print(p)
-   
-    # #print(ANS)
-    # K <- which.min(ANS)    
-    # #print(K/length(h_seq))  
-    # #print(K)
-    # message <- paste0("Оптимальное h =", K/10," для прямоугольного ядра")
-    # plot(h_seq, ANS/length(h_seq), xlab="Ширина окна", ylab="Вероятность ошибки", type="l")
+    plot(iris[, 3:4], pch = 21, bg = colo[iris$Species], col = colo[iris$Species], asp = 1)
+   for(i in 1:len){
+        if (p[i]!=0){
+            points(iris[i,3],iris[i,4],bg=colo[iris[i,5]], col=colo[iris[i,5]], asp = 1, cex=p[i]+5)
+        }
+}
     # points (K/10,ANS[K]/length(h_seq), pch = 21, bg = "red", col = "red",asp = 1)
     # label = paste0("h = ", K/10, "\n")
     # text(K/10,ANS[K]/length(h_seq), labels = label, pos = 4)
